dd153746b8bd083c3641861d09ab30f1b77dfbd3
LamWolf <lamwolfwang@gmail.com>
2019-08-02 18:07:02 +0800

    add check proxy module

--- bin/drop/drop-proxy.js
+++ bin/drop/drop-proxy.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 const program = require('commander');
 const Proxy = require('../../lib/drop/proxy');
+const detectInstalled = require('detect-installed');
+const chalk = require('chalk');
 
 program
     .allowUnknownOption()
@@ -10,7 +12,15 @@ program
     .option('-I --ignore-unauthorized-ssl', 'ignore rootCA security')
     .parse(process.argv);
 
-new Proxy()
+detectInstalled('anyproxy').then((exists) => {
+    if (exists) {
+        new Proxy()
+    } else {
+        console.log(`\ncommand not found: anyporxy\n\nPlease run ${chalk.yellow('npm install anyproxy -g')} first`);
+        process.exit(0);
+    }
+})
+
     
 process.on('SIGINT', () => {
     // 在添加了该事件（Ctrl+C）的监听之后，才能捕获到子进程对该事件做的操作返回
